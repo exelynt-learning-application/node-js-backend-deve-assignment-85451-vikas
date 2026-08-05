1e634a2a74ba4b87f65f129087e6591ca8961964
Bhagyashri-Dhavane <bhagyashri.dhavane@multigenesys.com>
2026-08-05 17:37:22 +0530

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,18 @@
+function add(a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new Error("Both inputs must be numbers.");
+  }
+  return a + b;
+}
+
+function subtract(a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new Error("Both inputs must be numbers.");
+  }
+  return a - b;
+}
+
+module.exports = {
+  add,
+  subtract
+};
