ee284754c39dbf9394f303d8b36e76a52fca5015
Bhagyashri-Dhavane <bhagyashri.dhavane@multigenesys.com>
2026-08-05 18:02:01 +0530

    Create Product.js

---
+++ Product.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  stock: {
+    type: Number,
+    default: 0
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: true
+  }
+});
+
+module.exports = mongoose.model("Product", productSchema);
